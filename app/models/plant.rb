9a7a254c483af3c2e660c0b1b4b981a99f832807
andre ferrer <andrerferrer@hotmail.com>
2020-11-11 14:25:57 -0300

    Finish the Plant model

---
+++ app/models/plant.rb
@@ -0,0 +1,6 @@
+class Plant < ApplicationRecord
+  belongs_to :garden
+
+  validates :name, presence: true, uniqueness: true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 puts "Cleaning the DB"
 Garden.destroy_all if Rails.env.development?
+Plant.destroy_all if Rails.env.development?
 
 puts "Create a garden"
 Garden.create!(
@@ -11,6 +12,22 @@ puts "Create another garden"
 Garden.create!(
   name: "My Other Garden",
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+
+puts "Create some plants"
+
+# create is new and save!
+Plant.create!(
+  name: "Orchid",
+  picture_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F546905948469887475%2F&psig=AOvVaw3Qp_kfgaN4CfhcW9nptojx&ust=1605201472946000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjp-s__-uwCFQAAAAAdAAAAABAD",
+  garden: Garden.first
+)
+
+Plant.create!(
+  name: "Rose",
+  picture_url: "https://images-na.ssl-images-amazon.com/images/I/71etj1mmV9L._AC_SL1500_.jpg",
+  garden: Garden.first
 )
 
 puts "Finished!
